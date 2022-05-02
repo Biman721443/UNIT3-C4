dd2400f0aa7efaf41afb2969f19ab67d11a434c0
Biman <bimangharai721443@gmail.com>
2022-05-02 12:31:29 +0530

    Add files via upload

---
+++ UNIT3 EVC4/unit-3-c4-template_685072/components/navbar.js
@@ -0,0 +1,26 @@
+// let navbar = () => {
+//     return ` <h1><a href="#">News App</a></h1>
+//     <input type="text" id="search_input" placeholder="search News" onkeydown="search(event)">`
+// }
+
+// export { navbar };
+
+
+// function navbar() {
+//     return
+
+//     `
+//     <h1><a href="#">News App</a></h1>
+//     <div> <input type="text" id="search_input" placeholder="search News" onkeydown="search(event)"> </div>
+//     `
+// }
+
+// export default navbar;
+
+
+let navbar = () => {
+    return `<h1><a href="#">News App</a></h1>
+    <input type="text" id="search_input" placeholder="search News" onkeydown="search(event)">`
+};
+
+export {navbar};
---
+++ UNIT3 EVC4/unit-3-c4-template_685072/scripts/index.js
@@ -0,0 +1,17 @@
+// Ude Import export (MANDATORY)
+// Onclicking store the news in local storage with key "news" so that you can access that on news.html page
+// import { navbar } from "../components/navbar.js";
+
+// let n = document.getElementById("navbar");
+// n.innerHTML = navbar();
+
+// import navbar from "../components/navbar.js";
+
+
+// document.getElementById("navbar").innerHTML = navbar()
+
+
+import { navbar } from "../components/navbar.js";
+
+let n= document.getElementById("navbar")
+n.innerHTML=navbar()
